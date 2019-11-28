888be88010b50c31f68c6136e047a67442307279
tiemimaeda <priscila.maeda@yahoo.com.br>
2019-11-28 17:44:11 -0300

    links ok

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const mdLinks = require("./lib/index.js");
+const file = "README.md";
+// const file = process.argv[2];
+
+mdLinks(file)
+  .then((result) => {
+    result.forEach(el => {
+      console.log(el.title, el.href);
+    });
+  })
+  .catch((resolve) => console.log(resolve))
